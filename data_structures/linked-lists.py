2e3a24a679176fb2840da576f96a4a23ab282245
Blake Sager <blake.sager@activeprospect.com>
2019-08-25 09:04:03 -0500

    implement feedback

--- data_structures/linked-lists.py
+++ data_structures/linked-lists.py
@@ -1,25 +1,22 @@
-class Node():
+class Node:
   def __init__(self, value, next_node=None):
     self.value = value
     self.next_node = next_node
 
+  def __str__(self):
+    return str(self.value)
 
-class LinkedList():
+
+class LinkedList:
   def __init__(self, head_node_value):
     self.head_node = Node(head_node_value)
 
+  def __str__(self):
+    return str([n.value for n in self.lazy_traversal()])
+
   def add_to_beginning(self, value):
     new_head = Node(value)
-    new_head.next_node = self.head_node
-    self.head_node = new_head
-
-  def stringify_list(self):
-    string = ''
-    current_node = self.head_node
-    while current_node:
-      string += f'{str(current_node.value)}\n'
-      current_node = current_node.next_node
-    return string
+    new_head.next_node, self.head_node = self.head_node, new_head
 
   def remove_node(self, value_to_remove):
     if self.head_node.value == value_to_remove:
@@ -34,20 +31,21 @@ class LinkedList():
           current_node = current_node.next_node
 
   def contains_value(self, value_to_find):
-    current_node = self.head_node
+    return value_to_find in self.lazy_traversal()
+
+  def lazy_traversal(self, node=None):
+    current_node = node if node is not None else self.head_node
+    
     while current_node:
-      if current_node.value == value_to_find:
-        return True
-      else:
-        current_node = current_node.next_node
-    return False
+      yield current_node
+      current_node = current_node.next_node
 
 ll = LinkedList(24)
 ll.add_to_beginning(46)
 ll.add_to_beginning(84)
-print(ll.stringify_list())
+print(ll)
 ll.remove_node(46)
-print(ll.stringify_list())
+print(ll)
 
 print(ll.contains_value(24))
 print(ll.contains_value(46))
